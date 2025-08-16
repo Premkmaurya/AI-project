b72ce5d42eedef3d3c0cf657c73d4df4c4500116
Premkmaurya <premkmaurya123456@gmail.com>
2025-08-15 21:54:55 -0700

    login functionality

--- chat-gpt/server.js
+++ chat-gpt/server.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const app = require('./src/app')
 const connectDb = require('./src/db/db')
+const setupSocketServer = require('./src/sockets/auth.socket');
+const http = require('http')
 
+const httpServer = http.createServer(app)
 
+setupSocketServer(httpServer)
 connectDb()
 
-app.listen(3000,()=>{
+httpServer.listen(3000,()=>{
 	console.log('server is listen at prot 3000.')
 })
---
+++ chat-gpt/src/sockets/auth.socket.js
@@ -0,0 +1,16 @@
+const {Server} = require('socket.io');
+
+function setupSocketServer(httpServer) {
+	const io = new Server(httpServer,{});
+
+	io.on('connection',(socket)=>{
+		console.log('server is connected.');
+
+
+		socket.on('disconnect',(socket)=>{
+			console.log('server is disconnected.')
+		})
+	})
+}
+
+module.exports = setupSocketServer;
