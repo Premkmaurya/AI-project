5f45703786d341fa0bb06d0a6aa74ffe4d6bfa8e
Premkmaurya <premkmaurya123456@gmail.com>
2025-08-22 00:34:07 -0700

    ai implimantation

--- chat-gpt/src/sockets/socket.server.js
+++ chat-gpt/src/sockets/socket.server.js
@@ -2,6 +2,8 @@ const {Server} = require('socket.io');
 const cookie = require('cookie');
 const jwt = require('jsonwebtoken');
 const userModel = require('../models/user.model')
+const generateResponse = require('../services/ai.service')
+const messageModel = require('../models/message.model')
 
 function setupSocketServer(httpServer) {
 	const io = new Server(httpServer,{});
@@ -29,8 +31,40 @@ function setupSocketServer(httpServer) {
 
 	io.on('connection',(socket)=>{
 
-		console.log(socket)
+		socket.on('ai-message',async(messagePayload)=>{
+			
+            await messageModel.create({
+            	chat:messagePayload.chatId,
+            	user:socket.user._id,
+            	content:messagePayload.content,
+            	role:"user"
+            })
 
+            const chatHistory = (await messageModel.find({
+                      chat:messagePayload.chatId
+              }).sort({createdAt:-1}).limit(20).lean()).reverse()
+
+
+			const response = await generateResponse(chatHistory.map(item=>{
+				return {
+					role:item.role,
+					parts:[{text:item.content}]
+				}
+			}))
+			
+			socket.emit('ai-response',{
+               content:response,
+               chatId:messagePayload.chatId
+			})
+
+			await messageModel.create({
+            	chat:messagePayload.chatId,
+            	user:socket.user._id,
+            	content:response,
+            	role:"model"
+            })
+
+		})
 
 		socket.on('disconnect',(socket)=>{
 			console.log('server is disconnected.')
