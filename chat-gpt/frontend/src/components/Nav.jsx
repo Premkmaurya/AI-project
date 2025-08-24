7f201d8e0b6218362a380836e99768f1f51c0c61
Premkmaurya <premkmaurya123456@gmail.com>
2025-08-24 13:38:08 +0530

    home page

---
+++ chat-gpt/frontend/src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+
+function Nav() {
+  return (
+  <div className="absolute w-full h-[5rem] bg-transparent flex items-center justify-center py-6">
+    <div className='bg-red-400 w-[40vw] h-[3.5rem] rounded-3xl text-white z-3 text-2xl'>
+        
+    </div>
+  </div>
+  )
+}
+
+export default Nav
--- chat-gpt/frontend/src/routes/AppRoutes.jsx
+++ chat-gpt/frontend/src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Routes,Route} from "react-router-dom";
 import Home from "../pages/Home"
+import Chat from "../pages/Chat"
 import Register from "../pages/Register"
 import Login from "../pages/Login"
 
@@ -8,6 +9,7 @@ function AppRoutes() {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
+			<Route path="/chat" element={<Chat />} />
 			<Route path="/register" element={<Register />} />
 			<Route path="/login" element={<Login />} />
 		</Routes>
