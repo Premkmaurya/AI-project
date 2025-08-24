6e4d2f719897d6153ff1bd8f01708db20aa58a14
Premkmaurya <premkmaurya123456@gmail.com>
2025-08-24 23:55:53 +0530

    landing page-2

--- chat-gpt/frontend/src/pages/Home.jsx
+++ chat-gpt/frontend/src/pages/Home.jsx
@@ -4,50 +4,55 @@ import Particle from "../components/Particle";
 import Nav from "../components/Nav";
 import gpt from '../assets/gpt.png'
 import HeroSection from "../components/HeroSection";
+import TextType from '../components/TextType'
 
 function Home() {
   const mainRef = useRef(null)
   const imgRef = useRef(null)
   const contentRef = useRef(null)
   useEffect(() => {
-    const tl = gsap.timeline()
-    tl.from(mainRef.current,{
-      y:"100%",
-      duration:0.8,
-      ease:"power2.out"
-    }),
-    tl.from(imgRef.current,{
-      scale:0,
-      opacity:0,
-      rotate:720,
-      delay:0.3,
-      duration:2,
-      ease:"power3.out"
-    }),
-    tl.set(imgRef.current,{
-      filter:"drop-shadow(4px 4px 12px rgba(255,255,255,0.4))"
-    }),
-    tl.to(imgRef.current,{
-      rotate:720,
-      scale:16,
-      duration:2,
-      ease:"power3.out",
-      onUpdate:function () {
-        const progress = this.progress();
-        if(progress >= 0.86){
-          mainRef.current.style.display = "none";
-          contentRef.current.style.display = 'block';
-        }
+    const tl = gsap.timeline({ defaults: { ease: "power3.out" } });
+
+    // Initial animations
+    tl.from(mainRef.current, {
+        y: "100%",
+        duration: 0.8,
+    })
+    .from(imgRef.current, {
+        scale: 0,
+        opacity: 0,
+        delay:0.4,
+        y:"50%",
+        rotate: 720,
+        duration: 2,
+    })
+    .to(imgRef.current, {
+        y: "20%",
+        opacity: 0,
+        scale: 0,
+        duration: 1.5,
+        onComplete: ()=>{
+        mainRef.current.style.display = "none";
+        contentRef.current.style.display = "block";
       }
     })
-  }, [])
+}, []);
 
   return (
-    <div className="overflow-hidden">
-    <div ref={mainRef} className="bg-black w-[100vw] flex justify-center items-center h-[100vh]">
+    <div className="overflow-hidden bg-black">
+    <div ref={mainRef} className="bg-black w-[100vw] flex flex-col justify-center items-center h-[100vh]">
       <img className="w-[6rem] h-[8rem]" ref={imgRef} src={gpt} />
+      <TextType 
+        text={["Introducing To you, ChatGPT.",""]}
+        typingSpeed={20}
+        showCursor={true}
+        pauseDuration={2500}
+        cursorCharacter="_"
+        className="text-md"
+        deletingSpeed={10}
+      />
     </div>
-    <div ref={contentRef} className="hidden relative bg-black overflow-hidden w-screen h-screen">
+    <div ref={contentRef} className="hidden absolute bg-black overflow-hidden w-screen h-screen">
       <Nav />
       <HeroSection />
       <Particle
