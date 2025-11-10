af70e3d5204419a6664ead159ff4438805c74ef0
Premkmaurya <premkmaurya123456@gmail.com>
2025-11-10 10:09:11 +0530

    Add Silk font file to the frontend public font directory

--- caption-generator/backend/src/app.js
+++ caption-generator/backend/src/app.js
@@ -1,25 +1,26 @@
-const express = require('express')
-const cookieParser = require('cookie-parser');
-const authRoutes = require('./routes/auth.routes')
-const postRoutes = require('./routes/post.routes')
-const cors = require('cors')
-const path = require('path');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/auth.routes");
+const postRoutes = require("./routes/post.routes");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials: true
-}))
-app.use('/api/auth',authRoutes)
-app.use('/api/posts',postRoutes)
-
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://ai-project-uho4.onrender.com/"],
+    credentials: true,
+  })
+);
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("*name",(req,res)=>{
-	res.sendFile(path.join(__dirname,"../public/index.html"))
-})
+app.get("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 module.exports = app;
